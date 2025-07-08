3f575f5053127dc7948799b8d852947f41ca53d0
Marcemmanuel1 <broumarc175@gmail.com>
2025-07-08 22:29:18 +0000

    premier commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["https://nexuchat.onrender.com", "http://127.0.0.1:5173"],
     methods: ["GET", "POST"],
     credentials: true
   }
@@ -36,7 +36,7 @@ const pool = mysql.createPool({
 
 // Middlewares
 app.use(cors({ 
-  origin: ["http://localhost:5173", "http://127.0.0.1:5173"], 
+  origin: ["https://nexuchat.onrender.com", "http://127.0.0.1:5173"], 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
